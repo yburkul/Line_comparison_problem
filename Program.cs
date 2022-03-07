900fcd631221f0369ff28d9a1c15f3efa19763fc
yburkul <yburkul123@gmail.com>
2022-03-07 17:24:26 +0530

    Added:Length of line

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Welcome to Line Comparison Computation Program");
+﻿using System;
+
+namespace Line_Comparison_Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1 = 12;
+            int x2 = 15;
+            int y1 = 10;
+            int y2 = 5;
+            var Length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("Length of Line: " + Length);
+        }
+    }
+}
