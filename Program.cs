abb170943b1a22aac5e747c1d43b30d74cec9788
yburkul <yburkul123@gmail.com>
2022-03-07 17:45:21 +0530

    Added:Check line is equal or not

--- Program.cs
+++ Program.cs
@@ -6,12 +6,28 @@ namespace Line_Comparison_Problem
     {
         static void Main(string[] args)
         {
-            int x1 = 12;
+            int x1 = 35;
             int x2 = 15;
-            int y1 = 10;
-            int y2 = 5;
-            var Length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-            Console.WriteLine("Length of Line: " + Length);
+            int y1 = 13;
+            int y2 = 10;
+            var Length_of_first_line = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("Length of Line: " + Length_of_first_line);
+
+            int x3 = 22;
+            int x4 = 18;
+            int y3 = 13;
+            int y4 = 10;
+            var Length_of_second_line = Math.Sqrt(Math.Pow(x4 - x3, 2) + Math.Pow(y4 - y3, 2));
+            Console.WriteLine("Length of Line: " + Length_of_second_line);
+
+            if (Length_of_first_line == Length_of_second_line)
+            {
+                Console.WriteLine("Length are equal");
+            }
+            else
+            {
+                Console.WriteLine("Length are not equal");
+            }
         }
     }
 }
