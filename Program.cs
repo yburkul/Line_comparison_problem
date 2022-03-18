fbc8b5e91be5b037dfdab090d7ce8eb9b46812e5
yburkul <yburkul123@gmail.com>
2022-03-18 18:25:45 +0530

    Added:Compare to methods

--- Program.cs
+++ Program.cs
@@ -2,23 +2,26 @@
 
 namespace Line_Comparison_Problem
 {
-    class Program
+    class Comparison
     {
-        static void Main(string[] args)
+        double Length_of_first_line;
+        double Length_of_second_line;
+        public void Calculation()
         {
             int x1 = 35;
             int x2 = 15;
             int y1 = 13;
             int y2 = 10;
-            var Length_of_first_line = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-            Console.WriteLine("Length of Line: " + Length_of_first_line);
+
+            Length_of_first_line = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("Length of First Line: " + Length_of_first_line);
 
             int x3 = 22;
             int x4 = 18;
             int y3 = 13;
             int y4 = 10;
-            var Length_of_second_line = Math.Sqrt(Math.Pow(x4 - x3, 2) + Math.Pow(y4 - y3, 2));
-            Console.WriteLine("Length of Line: " + Length_of_second_line);
+            Length_of_second_line = Math.Sqrt(Math.Pow(x4 - x3, 2) + Math.Pow(y4 - y3, 2));
+            Console.WriteLine("Length of Second Line: " + Length_of_second_line);
 
             if (Length_of_first_line == Length_of_second_line)
             {
@@ -37,6 +40,17 @@ namespace Line_Comparison_Problem
             {
                 Console.WriteLine("First line is less than second line");
             }
+            Console.WriteLine("Comparison of two length: " + Length_of_first_line.CompareTo(Length_of_second_line));
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comparison comparison = new Comparison();
+            comparison.Calculation();
         }
     }
 }
+
+
